cda0cc74d1e5086df66e3ec6bf113c0caf78cc7a
Larisa Zubkova <larissazubkova@gmail.com>
2024-01-30 13:16:41 +0300

    Реализует слайдер-карусель похожие товары

--- src/components/similar-list/similar-list.tsx
+++ src/components/similar-list/similar-list.tsx
@@ -2,6 +2,7 @@ import { CameraCard } from '../../types/product';
 import { useState } from 'react';
 import { SIMILAR_COUNT_STEP } from '../../const';
 import ProductCard from '../product-card/product-card';
+import './similar-list.css';
 
 type SimilarListProps = {
   products: CameraCard[];
@@ -23,7 +24,7 @@ function SimilarList({products}: SimilarListProps): JSX.Element {
               {currentSimilar.map((product) => <ProductCard key={product.id} product={product} isActive/>)}
             </div>
             <button
-              className="slider-controls slider-controls--prev"
+              className="controls slider-controls--prev"
               type="button"
               aria-label="Предыдущий слайд"
               onClick={() => setSimilarCount((prev) => prev - SIMILAR_COUNT_STEP)}
@@ -34,7 +35,7 @@ function SimilarList({products}: SimilarListProps): JSX.Element {
               </svg>
             </button>
             <button
-              className="slider-controls slider-controls--next"
+              className="controls slider-controls--next"
               type="button"
               aria-label="Следующий слайд"
               onClick={() => setSimilarCount((prev) => prev + SIMILAR_COUNT_STEP)}

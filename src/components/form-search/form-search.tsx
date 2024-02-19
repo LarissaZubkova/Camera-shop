f14f88c46551f5e2f1f6db8f3ac30e289bd3fb12
Larissa <larissazubkova@gmail.com>
2024-02-19 22:37:03 +0300

    Добавит компонент формы поиска

---
+++ src/components/form-search/form-search.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useState } from 'react';
+import { useAppSelector } from '../../hooks';
+import { getProducts } from '../../store/product-process/product-process.selectors';
+import { CameraCard } from '../../types/product';
+import classNames from 'classnames';
+
+function FormSearch(): JSX.Element {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchedProducts, setSearchedProducts] = useState<CameraCard[] | null>(null);
+  const products = useAppSelector(getProducts);
+
+  const handlerInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(evt.currentTarget.value);
+    setSearchedProducts(() => products.filter((product) => product.name.includes(searchValue)));
+  };
+
+  return (
+    <div className={classNames('form-search', {'list-opened': searchedProducts})}>
+      <form>
+        <label>
+          <svg className="form-search__icon" width={16} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-lens"></use>
+          </svg>
+          <input
+            className="form-search__input"
+            type="text"
+            autoComplete="off"
+            placeholder="Поиск по сайту"
+            value={searchValue}
+            onChange={handlerInputChange}
+          />
+        </label>
+        <ul className="form-search__select-list">
+          {searchedProducts && searchedProducts.map((product: CameraCard) => <li key={product.id} className="form-search__select-item" tabIndex={0}>{product.name}</li>)}
+          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 8i</li>
+          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 7i</li>
+          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 6i</li>
+          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 5i</li>
+          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 4i</li>
+        </ul>
+      </form>
+      <button className="form-search__reset" type="reset">
+        <svg width={10} height={10} aria-hidden="true">
+          <use xlinkHref="#icon-close"></use>
+        </svg><span className="visually-hidden">Сбросить поиск</span>
+      </button>
+    </div>
+  );
+}
+
+export default FormSearch;
